8ce37009c0c4deace5697189caabe53fd1d00d85
ling <lingt.xyz@gmail.com>
2018-10-30 12:26:45 -0400

    move to root directory

--- A2/MockHttpServer.py
+++ A2/MockHttpServer.py
@@ -1,31 +1,35 @@
 import socket
 import threading
 import logging, sys
+import argparse
+
+# from fileType import dealFile
 
 class MockHttpServer:
 
 	BUFFER_SIZE = 1024 # 1 KiB
 	#HOST = 'localhost'
 
-	def __init__(self, port=8080):
+	def __init__(self, port=8080, d="."):
 		self.port = port
-		self.webRoot = 'data'
+		self.dataDirectory = d
 
 	def start(self):
-		print("Starting web server...")
+		logging.info("Starting web server...")
 		listener = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
 			listener.bind(('', self.port))
 			listener.listen(5)
-			print('Web server is listening at', self.port)
+			logging.info('Web server is listening at {}.'.format(self.port))
 			while True:
 				(client, address) = listener.accept()
 				logging.debug("Received a connection from {0}".format(address))
 				threading.Thread(target=self.response, args=(client, address)).start()
+
 		finally:
-			print("Shuting down the server...")
+			logging.info("Shuting down the server...")
 			listener.close()
-			print('Web server has been shut down at', self.port)
+			logging.info('Web server has been shut down at {}.'.format(self.port))
 
 	def response(self, client, address):
 		# convert bytes to string
@@ -38,6 +42,12 @@ class MockHttpServer:
 		logging.debug("Received the {0} request.".format(method))
 		if(method == "GET"):
 			logging.debug("Received the GET request.")
+			if(resource == "/"):
+				logging.debug("Get file list")
+			elif(resource == ""):# /foo
+				logging.debug("Get file content")
+			else:
+				logging.error("Unsupported request.")
 		elif(method == "POST"):
 			logging.debug("Received the POST request.")
 		else:
@@ -54,6 +64,17 @@ class MockHttpServer:
 				break
 		return data
 
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
-server = MockHttpServer()
+# Usage: python3 httpfs [-v] [-p PORT] [-d PATH-TO-DIR]
+parser = argparse.ArgumentParser()
+parser.add_argument("-v", help="output log", action='store_true')
+parser.add_argument("-p", help="server port", type=int, default=8080)
+parser.add_argument("-d", help="data directory", default=".")
+args = parser.parse_args()
+#run_client(args.host, args.port)
+
+if(args.v):# output debug
+	logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+else:
+	logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+server = MockHttpServer(args.p, args.d)
 server.start()
