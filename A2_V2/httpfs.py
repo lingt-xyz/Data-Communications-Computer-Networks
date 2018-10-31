8ca206c95b9d2448e7ea6c8120fd869250150474
TiantianJi <jtt81083@gmail.com>
2018-10-31 01:01:50 -0400

    -

--- A2_V2/httpfs.py
+++ A2_V2/httpfs.py
@@ -14,11 +14,11 @@ parser.add_argument("-d", help="data directory", default=".")
 args = parser.parse_args()
 #run_client(args.host, args.port)
 
- if(args.v):# output debug
-     logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+# run_client(args.host, args.port)
 
- else:
-     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-
+if (args.v):  # output debug
+    logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+else:
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 server = MockHttpServer(args.p, args.d)
 server.start()
