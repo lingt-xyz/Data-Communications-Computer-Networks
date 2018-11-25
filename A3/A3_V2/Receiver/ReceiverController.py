9a23f1810e44b243ac955a619a76ff0da6b2889e
TiantianJi <jtt81083@gmail.com>
2018-11-25 09:55:11 -0500

    -

---
+++ A3/A3_V2/Receiver/ReceiverController.py
@@ -0,0 +1,48 @@
+from socket import *
+from DealPackets.Packet import *
+from DealPackets.PacketDecoder import *
+from DealPackets.PacketBuilder import *
+from Receiver.SelectiveRepeatReceiver import *
+
+
+class ReceiverController:
+    address = None
+    __socketRC = None
+    __routerAddr = ('127.0.0.1', 3000)
+    __packetBuilder = None
+    __window = None
+    __port = None
+
+    def __init__(self, port):
+        self.__port = port
+
+    def sendPacket(self, packetType, sequenceNumber, content):
+        print("Sending packet type: " + str(packetType) + " with #" + str(sequenceNumber))
+        packet = self.__packetBuilder.build(packetType, sequenceNumber, content)
+        self.__socketRC.sendto(packet.getBytes(), self.__routerAddr)
+
+    def getPacket(self, timeout=None):
+        self.__socketRC.settimeout(timeout)
+       #TODO: decode pkt type and seq num
+
+    def buildConnection(self):
+        packet = self.getPacket()
+
+        #boolean if connection is built
+        #TODO: if pkt type is syn, send ack syn, if already acked, return true
+
+        return False
+
+    def getMessage(self):
+        self.__socketRC = socket(AF_INET, SOCK_DGRAM)
+        self.__socketRC.bind(('', self.__port))
+        print("Listening")
+
+        # Make sure we have some connection.
+        if (self.buildConnection()):
+
+            #TODO: if window not finished, keep doing till end, send ack pkt,
+
+            return self.__window.getMessage()
+
+
---
+++ A3/A3_V2/Sender/SenderController.py
@@ -0,0 +1,49 @@
+from socket import *
+from DealPackets.Packet import *
+from Sender.SelectiveRepeatSender import *
+
+
+class SenderController:
+    __window = None
+    __socket = None
+    __addr = None
+    __routerAddr = ('127.0.0.1', 3000)
+    __packetBuilder = None
+    __port = None
+
+    def __init__(self, ip, port):
+        if (str(ip).lower() == "localhost"):
+            ip = "127.0.0.1"
+
+        #self.__packetBuilder = pkt with ip and port TODO
+        self.__addr = (ip, port)
+
+    def sendMessage(self, message):
+        self.__socket = socket(AF_INET, SOCK_DGRAM)
+        self.__socket.settimeout(1)
+        self.__window = SenderWindow(message, self.sendPacket, self.getResponse)
+
+        if self.connect(self.__window.windowSize):
+            print("Connected")
+
+            while not self.__window.finished():
+                self.__window.process()
+        else:
+            print("Can't establish connection")
+
+        self.__port = self.__socket.getsockname()[1]
+        self.__socket.close()
+
+    def sendPacket(self, packetType, sequenceNumber, content):
+        print("Sending packet type: " + str(packetType) + " with #" + str(sequenceNumber))
+        packet = self.__packetBuilder.build(packetType, sequenceNumber, content)
+        self.__socket.sendto(packet.getBytes(), self.__routerAddr)
+
+    def getResponse(self):
+        #TODO:
+
+    def connect(self, windowSize):
+        #TODO:
+
+    def getSocketPort(self):
+        return self.__port
