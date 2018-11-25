bdd1b1ebff44362f139b2657535017c73765172e
TiantianJi <jtt81083@gmail.com>
2018-11-25 11:33:03 -0500

    Create packetConstructor.py

---
+++ A3/A3_V2/DealPackets/packetConstructor.py
@@ -0,0 +1,12 @@
+from DealPackets.Packet import *
+
+class PacketConstructor:
+        __destinationAddress = None
+        __destinationPort = None
+
+        def __init__(self, destinationAddress, destinationPort):
+                self.__destinationAddress = destinationAddress
+                self.__destinationPort = destinationPort
+
+        def build(self, packetType, sequenceNumber, payload):
+                return Packet(packetType, sequenceNumber, self.__destinationAddress, self.__destinationPort, payload)
