cc6e35dbbd6b4073ce8dc2cfde3126f515846634
Ling Tan <ta_li@mommsen.encs.concordia.ca>
2018-11-26 19:42:17 -0500

    add comment

--- A3/A3_V2/const.py
+++ A3/A3_V2/const.py
@@ -4,4 +4,10 @@ ROUTER_IP = "127.0.0.1"
 ROUTER_PORT = "3000"
 WINDOW_SIZE = 5
 TIME_OUT = 5
+
 PACKET_SIZE = 1024
+"""
+The maximum amount of data to be received at once is specified by bufsize.
+See https://docs.python.org/3/library/socket.html#socket.socket.recv
+
+"""
